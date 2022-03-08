1edff680752a62b88f75b06f49cd7297e5b538cd
hnsa9 <hanisa.nine@gmail.com>
2022-03-08 21:22:43 +0800

    create header component

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import mealsImage from '../../assets/meals.jpg';
+import classes from './Header.module.css';
+
+const Header = props => {
+    return <React.Fragment>
+        <header className={classes.header}>
+            <h1>My Restaurant</h1>
+            <button>Cart</button>
+        </header>
+        <div className={classes['main-image']}>
+            {/* <img src={mealsImage}/> - local image */}
+            <img src="https://raw.githubusercontent.com/academind/react-complete-guide-code/11-practice-food-order-app/extra-files/meals.jpg"
+             alt="Food" />            
+        </div>
+
+    </React.Fragment>
+};
+
+export default Header;
