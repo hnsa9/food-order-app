a379aa2aa58c76b92e933eab6ffc5c6703100048
hnsa9 <hanisa.nine@gmail.com>
2022-03-12 00:13:35 +0800

    add logic for add item and remove item in cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -9,9 +9,13 @@ const Cart = props => {
     const cartCtx = useContext(CartContext);
 
     
-    const cartItemRemoveHandler = id => {};
+    const cartItemRemoveHandler = id => {
+        cartCtx.removeItem(id);
+    };
 
-    const cartItemAddHandler = item => {};
+    const cartItemAddHandler = item => {
+        cartCtx.addItem({...item, amount:1});
+    };
 
     // use items from cartctx
     const cartItems = <ul className={classes['cart-items']}> {cartCtx.items.map(item => 
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -10,12 +10,65 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
     if (action.type === 'ADD') {
-        const updatedItems = state.items.concat(action.item);
+        
         const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount);
+        
+        // check if item is already is cart
+        // findindex - The findIndex() method returns the index of the first element in the array that satisfies the provided testing function. 
+        // Otherwise, it returns -1, indicating that no element passed the test.
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+
+        const existingCartItem = state.items[existingCartItemIndex];
+        
+        // let updatedItem;
+        let updatedItems;
+
+        // if existingcartitem is not null
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                amount: existingCartItem.amount + action.item.amount
+            };
+            // create new array by copying old objects
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            // updatedItem = {...action.item};
+            updatedItems = state.items.concat(action.item);
+        }
+
+        
         return {
             items: updatedItems,
             totalAmount: updatedTotalAmount
         };
+    }
+
+    if (action.type === 'REMOVE') {
+
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.id);
+        const existingItem = state.items[existingCartItemIndex];
+
+        const updatedTotalAmount = state.totalAmount - existingItem.price;
+
+        let updatedItems;
+        
+        // check if amount is 1 - if yes remove item totally, if not just update amount number
+        if (existingItem.amount === 1) {
+            updatedItems = state.items.filter((item) => item.id !== action.id) // return items with id not equal to action.id
+
+        } else {
+            const updatedItem = {...existingItem, amount: existingItem.amount - 1};
+            updatedItems = [...state.items];
+            updatedItems[existingCartItemIndex] = updatedItem;
+        }
+
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+
+    
     }
 
 
