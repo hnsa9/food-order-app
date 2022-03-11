6d3fff079dffe2a41e5f0a80ce58d40b8efffbce
hnsa9 <hanisa.nine@gmail.com>
2022-03-11 22:47:34 +0800

    display correct number of items in cart finished

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import { Fragment } from 'react';
+import  React, { useContext, Fragment } from 'react';
 import classes from './MealItem.module.css';
 import MealItemForm from './MealItemForm';
+import CartContext from '../../../store/cart-context';
 
 const MealItem = props => {
     // return <Fragment>
@@ -20,6 +20,19 @@ const MealItem = props => {
 
     const price = `RM${props.price.toFixed(2)}`;
 
+    const cartCtx = useContext(CartContext);
+
+    const addItemToCartHandler = amount => {
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        })
+
+
+    };
+
     return <li className={classes.meal}>
             <div>
             <h3>{props.name}</h3>
@@ -27,7 +40,7 @@ const MealItem = props => {
             <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm id={props.id}/>
+                <MealItemForm id={props.id} onAddToCart={addItemToCartHandler}/>
                 {/* form to enter amount and add to cart button */}
             </div>
         
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -1,13 +1,29 @@
 import classes from './Input.module.css';
+import React from "react";
 
-const Input = props => {
+const Input = React.forwardRef((props, ref) => {
+
+    // const inputRef = useRef();
+
+    // const activate = () => {
+    //     // inputRef.current.event.target.value();
+    //     console.log(inputRef.current.event.target.value());
+
+    // };
+
+    // useImperativeHandle(ref, () => {
+    //     return {
+    //         focus: activate //refer to function 
+    //     };
+    // })
+
     return <div className={classes.input}>
     <label htmlFor={props.input.id}>{props.label}</label>
-    <input {...props.input}/>
+    <input ref={ref} {...props.input}/>
     </div>
 
 
 
-};
+});
 
 export default Input;
