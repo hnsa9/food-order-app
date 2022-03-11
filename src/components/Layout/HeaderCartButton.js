9b9da5441422ca725941b9b00047fbbfe5fa7455
hnsa9 <hanisa.nine@gmail.com>
2022-03-12 00:30:14 +0800

    add animation to cart when add or remove item

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -3,7 +3,7 @@ import CartIcon from "../Cart/CartIcon";
 import classes from './HeaderCartButton.module.css';
 
 import CartContext from "../../store/cart-context";
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 // import CartProvider from "../../store/CartProvider";
 
 // button - icon, text, badge-current number of items in cart
@@ -11,13 +11,40 @@ import { useContext } from 'react';
 
 const HeaderCartButton = props => {
 
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
+    
+    const { items } = cartCtx;
+    
+
+    // animate cart button
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+  
+    useEffect(() => {
+      if (items.length === 0) {
+        return;
+      }
+      setBtnIsHighlighted(true);
+  
+      // unhighlight button after 300ms
+      const timer = setTimeout(() => {
+        setBtnIsHighlighted(false);
+      }, 300);
+      
+      // cleanup function - clear timer when it reruns
+      return () => {
+        clearTimeout(timer);
+      };
+    }, [items]);
+  
+
+    
 
     const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     }, 0); // reduce - single output
 
-    return <button className={classes.button} onClick={props.onClick}>
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}><CartIcon/></span>
         <span>Your Cart</span>
         <span className={classes.badge}>
